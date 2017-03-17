66463ec59964442824060cd8d3843503b3b713a0
tsukukobaan <yutaka.kobayashi@todai-dream-net.com>
2017-03-17 20:03:10 +0900

    minor changes

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -35,26 +35,28 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         
-//        let store = Store(context: context)
-//        store.name = "Best Buy"
-//        let store2 = Store(context: context)
-//        store2.name = "Tesla Dealership"
-//        let store3 = Store(context: context)
-//        store3.name = "Frys Electronics"
-//        let store4 = Store(context: context)
-//        store4.name = "Target"
-//        let store5 = Store(context: context)
-//        store5.name = "Amazon"
-//        let store6 = Store(context: context)
-//        store6.name = "K Mart"
-//        
-//        ad.saveContext()
+        let store = Store(context: context)
+        store.name = "Best Buy"
+        let store2 = Store(context: context)
+        store2.name = "Tesla Dealership"
+        let store3 = Store(context: context)
+        store3.name = "Frys Electronics"
+        let store4 = Store(context: context)
+        store4.name = "Target"
+        let store5 = Store(context: context)
+        store5.name = "Amazon"
+        let store6 = Store(context: context)
+        store6.name = "K Mart"
+        
+        ad.saveContext()
         
         getStores()
         
         if itemToEdit != nil {
             loadItemData()
         }
+        
+        hideKeyboard()
         
         
     
@@ -180,6 +182,23 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
     }
     
 
+}
+
+extension UIViewController
+{
+    func hideKeyboard()
+    {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(UIViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard()
+    {
+        view.endEditing(true)
+    }
 }
 
 
